5cb78fff4657f560180c6303353c334d5fe47afd
iamgeorgp <plus.george@yandex.ru>
2023-09-02 22:52:54 +0300

    v.2 add new funcs and work on async

--- bot.py
+++ bot.py
@@ -16,27 +16,27 @@ user_groups = {}
 
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("Перейти к инструкции для iOS", url='https://teletype.in/@geocode/Zj6q780IQAk')],
-        [InlineKeyboardButton("Меню", callback_data='show_menu')],
+        [InlineKeyboardButton("Read the iOS manual", url='https://teletype.in/@geocode/Zj6q780IQAk')],
+        [InlineKeyboardButton("Menu", callback_data='show_menu')],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
-        f"👋 Привет!\nБот сделан маёвцем для РЭУ.\n    Бот присылает расписание файлом формата .ics, который удобно импортировать в любой календарь (Google, Яндекс, Samsung, Apple, Outlook).\n    Для владельцев Android все работает сразу: файл календаря открывается через любое приложение.\n    Владельцам iOS необходимо сделать настройку, которая не займет больше 2 минут\n    Прочитать инструкцию или открыть меню ты можешь ниже по кнопкам:",
+        f"🎓 PRUE Study Buddy can help you add a schedule to your calendar app. \nIf you have Android installed, just click on the .ics file to import the schedule. For iOS users, follow the instructions to set up the import. \nReady to get started?",
         reply_markup=reply_markup
     )
 
 def menu(update: Update, context: CallbackContext) -> None:
     custom_keyboard = [
-        [KeyboardButton("Текущая неделя"), KeyboardButton("Следующая неделя")],
-        [KeyboardButton("Изменить группу")],
-        [KeyboardButton("Инструкция")],
+        [KeyboardButton("Current week"), KeyboardButton("Next week")],
+        [KeyboardButton("Change group")],
+        [KeyboardButton("iOS manual")],
     ]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
-    update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
+    update.message.reply_text("Select:", reply_markup=reply_markup)
 
 def send_instruction_link(update: Update, context: CallbackContext) -> None:
     instruction_link = "https://teletype.in/@geocode/Zj6q780IQAk"
-    update.message.reply_text("Вот инструкция для iOS", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Открыть инструкцию", url=instruction_link)]]))
+    update.message.reply_text("Here are the instructions for iOS", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open the manual", url=instruction_link)]]))
 
 
 def button_click(update: Update, context: CallbackContext) -> None:
@@ -45,36 +45,41 @@ def button_click(update: Update, context: CallbackContext) -> None:
         menu(query, context)
 
 def set_group(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Введите номер группы\nНапример: 15.06д-мен03а/21б")
+    update.message.reply_text("Enter the group number.\nFor example: 15.06д-мен03а/21б")
     # Настройте следующий обработчик для ожидания номера группы
     context.user_data['waiting_for_group'] = True
 
 def handle_group_number(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
-    if 'waiting_for_group' in context.user_data and pars_func.check_group(update.message.text):
+    if 'waiting_for_group' in context.user_data and pars_func.check_group(update, update.message.text):
         group_number = update.message.text
         user_groups[user_id] = group_number
         del context.user_data['waiting_for_group']
-        update.message.reply_text(f"✅Группа успешно установлена✅\n{group_number}")
+        update.message.reply_text(f"✅ The group has been successfully selected\n{group_number}")
     else:
-        update.message.reply_text(f"❌Неправильно введена группа❌\nНажмите еще раз на кнопку 'Изменить группу'")
-        # Введите команду 'Изменить группу', чтобы установить новую группу.
+        update.message.reply_text(f"❌ Incorrect group entered\nClick the 'Change group' button again")
+        # Введите команду 'Change group', чтобы установить новую группу.
 
 def send_current_schedule(update: Update, context: CallbackContext) -> None:
-    pars_func.hihi()
 
     user_id = update.message.from_user.id
     if user_id in user_groups:
         group_number = user_groups[user_id]
         pars_func.send_schedule(update, user_groups, user_id)
     else:
-        update.message.reply_text("Для начала установите номер своей группы командой 'Изменить группу'.")
+        update.message.reply_text("First, set your group number with the 'Change group' command.")
 
 def send_next_schedule(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Функция находится в разработке.")
-
+    update.message.reply_text("The feature is in development...")
+    # user_id = update.message.from_user.id
+    # if user_id in user_groups:
+    #     group_number = user_groups[user_id]
+    #     pars_func.send_n_schedule(update, user_groups, user_id)
+    # else:
+    #     update.message.reply_text("First, set your group number with the 'Change group' command.")
 
 def main():
+
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
@@ -88,16 +93,16 @@ def main():
     dispatcher.add_handler(CommandHandler('menu', menu))
 
     # Обработчик для кнопки "Инструкция"
-    dispatcher.add_handler(MessageHandler(Filters.text("Инструкция"), send_instruction_link))
+    dispatcher.add_handler(MessageHandler(Filters.text("iOS manual"), send_instruction_link))
 
     # Обработчик для команды "Изменить группу"
-    dispatcher.add_handler(MessageHandler(Filters.text("Изменить группу"), set_group))
+    dispatcher.add_handler(MessageHandler(Filters.text("Change group"), set_group))
 
     # Обработчик для кнопки "Текущая"
-    dispatcher.add_handler(MessageHandler(Filters.text("Следующая неделя"), send_next_schedule))
+    dispatcher.add_handler(MessageHandler(Filters.text("Next week"), send_next_schedule))
     
     # Обработчик для кнопки "Текущая"
-    dispatcher.add_handler(MessageHandler(Filters.text("Текущая неделя"), send_current_schedule))
+    dispatcher.add_handler(MessageHandler(Filters.text("Current week"), send_current_schedule))
 
     
 
@@ -107,7 +112,7 @@ def main():
     
 
 
-    updater.start_polling()
+    updater.start_polling()  # Здесь используется await
     updater.idle()
 
 if __name__ == '__main__':
